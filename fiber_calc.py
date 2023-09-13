e1808e72a9e717cd530d9e4de1c1821a0c4686d8
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-09-13 18:19:29 -0400

    Update fiber_calc.py

--- fiber_calc.py
+++ fiber_calc.py
@@ -13,23 +13,35 @@
 # Change Approved by: Ruben Brionez Jr
 # Date Moved to Production: 09/09/2023
 
+# Change Log
+# Change :2
+# Changes Made: Implemented an if loop to account for bulk discount pricing
+# Date of Change: 09/13/2023
+# Author: Ruben Brionez Jr
+# Change Approved by: Ruben Brionez Jr
+# Date Moved to Production: 09/17/2023
+
 try:
     print("****Welcome to the Calculator****")
     company = input("Please enter the company name:\n")  # This line requests a company name be entered
     feet_install = (float(input("Please enter the footage for the cable.\n")))  # This line requests an input footage
     # and converts to a float
     if feet_install > 500:
-        cost = feet_install * 0.50
+        price = 0.50
+        cost = feet_install * price
     elif feet_install > 250:
-        cost = feet_install * 0.70
+        price = 0.70
+        cost = feet_install * price
     elif feet_install > 100:
-        cost = feet_install * 0.80
+        price = 0.80
+        cost = feet_install * price
     else:
-        cost = feet_install * 0.87
+        price = 0.87
+        cost = feet_install * price
     round(cost, 2)  # This should round the final cost to two decimal places
     print("**************************************************")  # Line seperator
     print(f"The Company name entered was '{company}'\nThe total footage entered was {feet_install} FT\n"
-          f"The price per foot is $0.87\nThe total cost is ${cost}")
+          f"The price per foot is ${price}\nThe total cost is ${cost}")
     print("**************************************************")  # Line seperator
     print("Thank you!")
 except Exception:
