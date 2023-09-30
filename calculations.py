d9e1259719a8ecde1e7bfdab1aaaba0f540f4085
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-09-29 21:20:39 -0400

    Update calculations.py
    Added more comments and updated change log

--- calculations.py
+++ calculations.py
@@ -12,8 +12,17 @@
 # Change Approved by: Ruben Brionez Jr
 # Date Moved to Production: 10/01/2023
 
+# Change Log
+# Change :2
+# Changes Made: Defined two functions outside of main function
+# Date of Change: 09/29/2023
+# Author: Ruben Brionez Jr
+# Change Approved by: Ruben Brionez Jr
+# Date Moved to Production: 10/01/2023
 
-def perform_calculation():
+
+def perform_calculation():  # This function takes an operation parameter and then requests
+    # two number to perform the operation
     print("Enter two number")
     num1 = int(input("First number: "))
     num2 = int(input("Second number: "))
@@ -22,7 +31,7 @@ def perform_calculation():
 
 def calculate_average():
     lst = []
-    n = int(input("Enter How many number to be averaged: "))
+    n = int(input("Enter How many numbers to be averaged: "))
     for i in range(0, n):
         num = int(input("Please enter a number: "))
         lst.append(num)
@@ -32,19 +41,20 @@ def calculate_average():
         total = total + num
     print(f"The sum of the numbers entered is {total}")
     average = total / n
-    print(f"The average of the numbers entered is {average}")
+    print(f"The average of the numbers entered is {average:.2f}")
 
 
 def main():
     try:
         #  perform_calculation()
-        answer = input("Would you like to begin? Yes or No\n ")
-        while answer != "No":
+        answer = input("Would you like to begin? Yes or No\n")
+        while answer != "No" or "N":
             calculate_average()
-            answer = input("Would you like to continue? Yes or No\n ")
+            print("--------------------------------------------------")
+            answer = input("Would you like to continue? Yes or No\n")
 
     except Exception:
-        print("End of Main Function")
+        print("Please be sure to only enter numbers in the fields\nYou have existed the program, please run again")
 
 
 if __name__ == "__main__":
