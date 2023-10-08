57e4fa0c35dfc89b37d03f4b5854e9f404cea773
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-10-08 17:35:13 -0400

    Update temps.py
    Updated the temp file with additional comments. Revisit required for input handling.

--- temps.py
+++ temps.py
@@ -16,22 +16,28 @@ temperatures = []  # This creates an empty list to store input values
 
 def main():
     try:
-        print("Please enter a data set of temperatures.\nWhen completed, please enter '*' to save your list.")
-        print("_______________________________________________________")
+        print("Welcome to the Temperature Tracker!")
+        print("Please enter a data set of temperatures.\nWhen completed, please enter '*' to save your list.")  # This
+        # line  denotes the sentinel value to be used to exit the loop
+        print("Please only enter numbers!")
+        print("_______________________________________________________")  # break line
         while True:
             temps = input("Please enter a temperature reading in Fahrenheit degrees: ")
-            if temps == "*":
-                print("That completes the data set!")
-                break
+            if temps == "":
+                print("Please do not leave empty!")  # Catches an empty input value
             elif temps != "*":
-                temperatures.append(temps)
-                #  print(temperatures)
+                temperatures.append(temps)  # This adds the input value to the list
             else:
-                print("Make sure you entered a correctly formatted temperature!")
                 break
-        print(f"There were {len(temperatures)} entries in the list of temperatures")
-        print(f"The highest temperature entered was {max(temperatures)}")
-        print(f"The lowest temperature entered was {min(temperatures)}")
+        print(f"The temperatures entered were {temperatures}")  # This prints the temperatures
+        # entered as required by the assignment guidelines.
+        print(f"There were {len(temperatures)} entries in the list of temperatures")  # This prints how many
+        # temperatures entered as required by the assignment guidelines
+        print(f"The highest temperature entered was {max(temperatures)} degrees")  # This prints the max
+        # temperatures entered as required by the assignment guidelines
+        print(f"The lowest temperature entered was {min(temperatures)} degrees")  # This prints the min
+        # temperatures entered as required by the assignment guidelines
+
     except ValueError:
         print("Oops!  That was not a valid number.  Try again...")
 
