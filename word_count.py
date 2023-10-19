c5c47260776b0aa717bbd60812a5dfec72570e47
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-10-19 19:05:05 -0400

    Created a text file and new assignment file
    Created text file and .py assignment file

---
+++ word_count.py
@@ -0,0 +1,34 @@
+# DSC 510
+# Week 8
+# Programming Assignment 8.1 Week 8
+# Author Ruben Brionez Jr
+# 10/19/2023
+
+# Change Log
+# Change :1
+# Changes Made: Created Program
+# Date of Change: 10/19/2023
+# Author: Ruben Brionez Jr
+# Change Approved by: Ruben Brionez Jr
+# Date Moved to Production: 10/22/2023
+
+def add_word():
+    pass
+
+
+def process_line():
+    pass
+
+
+def pretty_print():
+    pass
+
+
+def main():
+    gba_file = open('gettysburg.txt', 'r')
+    for line in gba_file:
+        pass
+
+
+if __name__ == "__main__":
+    main()
