cd85e815901e8cc309a1bcfc762d89f73aa2e630
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-10-25 21:13:46 -0400

    Updating word_count_file
    Continued updating the program which will then be modified to write to a file

--- Python Files/word_count_to_file.py
+++ Python Files/word_count_to_file.py
@@ -29,3 +29,34 @@ def process_line(line, counts):  # This processes each line if the text file, th
     line = line.lower()  # This line converts all capital letters to lowercase
     words = line.split()  # This splits the words in the lines to be counted
     add_word(words, counts)  # This line calls the function to calculate the times a word appears
+
+
+def proces_file(counts):  # This line is intended to create a more readable output from the dictionary
+    len_pairs = []  # Created an empty list
+    for key, val in list(counts.items()):  # This loop places the key:value pair in a list in order to get a total
+        len_pairs.append((val, key))  # adds the key,value par to the empty list
+        total = len(len_pairs)  # totals the key/value paris in the list (This would be the total
+        # unique values in the the .txt file
+    print(f"Length of the dictionary is: {total}")
+    column1 = "Word"
+    column2 = "Count"
+    print(f"{column1}{column2:>20}")
+    print("------------------------")
+    len_pairs.sort(reverse=True)
+    for key, val in len_pairs:
+        print(f"{val:<12}{key:>10}")
+
+
+def main():
+    try:
+        gba_file = open('gettysburg.txt', 'r')  # Opens desired file
+        counts = dict()  # creates an Empty dictionary
+        for line in gba_file:
+            process_line(line, counts)
+        pretty_print(counts)
+    except FileNotFoundError:
+        print("File cannot be opened\nPlease be sure the file path is correct!")
+
+
+if __name__ == "__main__":
+    main()
