e0b26feaa0cd005ac01663904acefdc48ce74c53
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-11-03 13:09:32 -0400

    Create chuck_norris.py
    Created the program

---
+++ Python Files/chuck_norris.py
@@ -0,0 +1,14 @@
+# DSC 510
+# Week 10
+# Programming Assignment 10.1 Week 10
+# Author Ruben Brionez Jr
+# 11/05/2023
+
+# Change Log
+# Change :1
+# Changes Made: Created Program
+# Date of Change: 11/03/2023
+# Author: Ruben Brionez Jr
+# Change Approved by: Ruben Brionez Jr
+# Date Moved to Production: 11/05/2023
+
