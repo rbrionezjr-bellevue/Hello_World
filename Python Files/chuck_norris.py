4b96105c1efb256b604b043ba89c31dc237e4403
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-11-05 09:13:57 -0500

    Update chuck_norris.py
    Updated the program with two functions and a main function. The program uses an API to generate Chuck Norris jokes based on user input.

--- Python Files/chuck_norris.py
+++ Python Files/chuck_norris.py
@@ -12,3 +12,57 @@
 # Change Approved by: Ruben Brionez Jr
 # Date Moved to Production: 11/05/2023
 
+import requests
+import json
+from urllib.error import HTTPError
+
+
+def categories():
+    available_category = "https://api.chucknorris.io/jokes/categories"
+    response = requests.get(available_category)
+    api_data = response.json()
+    api_dictionary = {key: value for key, value in enumerate(api_data, 1)}
+    for key, value in api_dictionary.items():
+        print(key, value)
+    return api_dictionary
+
+
+def joke(value):
+    selected_category = f"https://api.chucknorris.io/jokes/random?category={value}"
+    response = requests.get(selected_category)
+    joke_data = response.json()
+    joke_phrase = joke_data.get("value")
+    print(joke_phrase)
+
+
+def main():
+    try:
+        print("Welcome to the Chuck Norris Joke Generator!")
+        while True:
+            answer = input("Would you like a Chuck Norris Joke? Yes or No: ")
+            answer = answer.lower()
+            if answer == "yes":
+                choices = categories()
+                key = int(input("Please enter the number for the category: "))
+                if key in range(1, 16):
+                    value = choices.get(key)
+                    print(f"You have selected the {value} category, enjoy this {value} Chuck Norris joke!")
+                    print("--------------------------------------------------------------------------------")
+                    joke(value)
+                    print("--------------------------------------------------------------------------------")
+                else:
+                    print("Make sure you entered a number that corresponds to a category!")
+            elif answer == "no":
+                print("MAYBE NEXT TIME!")
+                break
+            else:
+                print("Be sure to type 'Yes' or 'No'")
+    except HTTPError as err:
+        if err.code == 404:
+            print("The webpage may have been moved, verify in browser")
+        else:
+            raise
+
+
+if __name__ == "__main__":
+    main()
