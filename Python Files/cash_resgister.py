dcd98ba0225511adda808e7d6303cea61d7a1961
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-11-10 10:22:51 -0500

    Create cash_resgister.py
    Creating the cash_register program

---
+++ Python Files/cash_resgister.py
@@ -0,0 +1,26 @@
+# DSC 510
+# Week 10
+# Programming Assignment 10.1 Week 10
+# Author Ruben Brionez Jr
+# 11/12/2023
+
+# Change Log
+# Change :1
+# Changes Made: Created Program
+# Date of Change: 11/08/2023
+# Author: Ruben Brionez Jr
+# Change Approved by: Ruben Brionez Jr
+# Date Moved to Production: 11/12/2023
+
+
+class CashRegister:
+    def add_item(self, price):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
