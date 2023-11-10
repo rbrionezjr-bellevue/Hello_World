1e0b82570a6da400fdca5ab2e43129e32291189d
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-11-10 14:14:40 -0500

    Update cash_resgister.py
    Updating Cash_Register with the start of the code, unusable in current state. Additional programming required.

--- Python Files/cash_resgister.py
+++ Python Files/cash_resgister.py
@@ -12,13 +12,30 @@
 # Change Approved by: Ruben Brionez Jr
 # Date Moved to Production: 11/12/2023
 
-
 class CashRegister:
+    #  Define a class variable somewhere for a available fruits and their prices?
+
+    def __int__(self, price):
+        self.price = price
+
     def add_item(self, price):
-        pass
+        cart = []
+
+        
+welcome_message = "Welcome to the Fruit Cart!"
 
 
 def main():
+    price = 0
+    print(welcome_message.upper())
+    answer = input("Would you like to add items to your cart? Yes or No: ")
+    answer = answer.lower()
+    if answer == "yes":
+        item = input("Please enter a fruit: ")
+        while item != "stop":
+            CashRegister().add_item(price)
+    else:
+        print("Thanks for stopping by!")
     pass
 
 
