df4d01727f9f12c1f3f4f690becf3bb91fca2dd9
rbrionezjr-bellevue <rbrionezjr@my365.bellevue.edu>
2023-12-09 13:02:09 -0500

    Update assignment_00.R
    Update to the assignment with additional output

--- R Programming/assignment_00.R
+++ R Programming/assignment_00.R
@@ -27,18 +27,22 @@ print(x)
 ## Assign the value 41 to the variable y
 ## Print y
 y <- 41
+print(y)
 
 ## Assign the output of x + y to the variable z
 ## Print z
 z <- x + y
+print(z)
 
 ## Assign the string value "DSC520" to the variable class_name
 ## Print the value of class_name
 class_name <- "DSC520"
+print(class_name)
 
 ## Assign the string value of TRUE to the variable is_good
 ## Print the value of is_good
 is_good <- "TRUE"
+print(is_good)
 
 ## Check the class of the variable is_good using the `class()` function
 class(is_good)
